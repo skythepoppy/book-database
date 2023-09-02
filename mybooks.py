c984b61120b28a630f4cc8b7435256d7740d56dd
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-09-01 22:06:46 -0500

    created gui and customized theme

---
+++ mybooks.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Button,Label,Scrollbar,Listbox,StringVar,Entry,W,E,N,S,END
+from tkinter import ttk
+from tkinter import messagebox
+
+root = Tk()
+
+root.title("My Books Database Application")
+root.configure(background='light blue')
+root.geometry("950x550")
+root.resizable(width=False, height=False)
+
+
+root.mainloop()
